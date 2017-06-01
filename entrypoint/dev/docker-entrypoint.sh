d682cb107bae32f2c134cbbef442ef6277cbf41e
Kamil Zajac <kaz231@outlook.com>
2017-06-01 18:47:47 +0000

    - shadow package was added for dev image - app's user is created in Dockerfile instead of entrypoint - usdermod was used to imitate owner of volume

--- entrypoint/dev/docker-entrypoint.sh
+++ entrypoint/dev/docker-entrypoint.sh
@@ -6,13 +6,11 @@ if [ -z "$(ls -A $APP_DIR)" ]; then
   exit 1
 fi
 
-# get gid and uid of host user that is an owner of volume's files
+# get uid of host user that is an owner of volume's files
 VOLUME_UID=$(stat -c "%u" "$APP_DIR")
-VOLUME_GID=$(stat -c "%g" "$APP_DIR")
 
-# create group and user using fetched gid and uid
-addgroup -g "$VOLUME_GID" "$APP_USER_GROUP"
-adduser -u "$VOLUME_UID" -G "$APP_USER_GROUP" -D "$APP_USER"
+# change user's uid to owner of volume
+usermod -u "$VOLUME_UID" "$APP_USER"
 
 # run given command as $APP_USER
 su-exec "$APP_USER" "$@"
